ac1e5e6a41e47baf4abdc877f728a780b936d69b
andreabenf <andreabenf@gmail.com>
2019-04-20 15:54:49 -0300

    Código está funcionando, Adicionando um usuário e tratando possiveis erros

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : false}));
 
+
 require('./controllers/authControler')(app);
 
-app.listen(3030);
+app.listen(3031);
--- src/models/user.js
+++ src/models/user.js
@@ -23,14 +23,12 @@ const UserSchema = new mongoose.Schema({
     },
 });
 
-
-
-UserSchema.pre('save', async function (next) {
-    const hash = await bcrypt.hash(this.password, 10);
+UserSchema.pre('save', async function(next) {
+    const hash =  await bcrypt.hash(this.password,10);
     this.password = hash;
-
     next();
 });
+
 const User = mongoose.model('User', UserSchema);
 
 module.exports = User;
