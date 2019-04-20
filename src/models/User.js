6e282f46c75af88335a5db8adc6732629f1d1fe1
andreabenf <andreabenf@gmail.com>
2019-04-19 23:14:20 -0300

    mais

--- src/models/User.js
+++ src/models/User.js
@@ -1,11 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require('../database');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
     name: {
         type: String,
         require: true,
     },
-    email : {
+    email: {
         type: String,
         unique: true,
         required: true,
@@ -21,3 +22,15 @@ const UserSchema = new mongoose.Schema({
         default: Date.now,
     },
 });
+
+
+
+UserSchema.pre('save', async function (next) {
+    const hash = await bcrypt.hash(this.password, 10);
+    this.password = hash;
+
+    next();
+});
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
